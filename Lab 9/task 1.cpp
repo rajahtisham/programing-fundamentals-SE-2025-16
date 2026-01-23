424c2b47f5b4847b449f7e1e2773259c39640f36
Raja Ihtisham <rajaihtisham82@gmail.com>
2026-01-23 00:08:27 -0800

    Add files via upload

---
+++ Lab 9/task 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    int i = 1;
+
+    cout << "Enter a number: ";
+    cin >> N;
+
+    while (i <= N) {
+        cout << i << " ";
+        i++; 
+    }
+
+    return 0;
+}
+
