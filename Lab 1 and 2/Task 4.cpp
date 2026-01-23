9fe64996eacd92acea0afe7d88412b2d75ab9f82
Raja Ihtisham <rajaihtisham82@gmail.com>
2026-01-23 04:37:03 +0000

    Add files via upload

---
+++ Lab 1 and 2/Task 4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Let's learn C++! \n";
+	
+	
+	cout << "To start, save your file in C:\\C++_Projects\\Lab1\n";
+	
+	
+	
+	cout << "Good Luck!\n";
+	
+	
+	
+	return 0;
+}
---
+++ Lab 1 and 2/Task3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout <<"Item\tPrice\tQuality\n";
+	cout <<"Pencil\t$0.5\t5\n";
+	cout <<"Scale\t$0.1\t15\n";
+	cout << "Eraser\t0.3\t10" << endl;
+	return 0;
+	
+}
